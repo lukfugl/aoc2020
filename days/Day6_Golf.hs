6c099d7386f1782b5316b178bab44df003243330
Jacob Fugal <lukfugl@gmail.com>
2020-12-05 22:36:10 -0700

    improve golfed version of day6

--- days/Day6_Golf.hs
+++ days/Day6_Golf.hs
@@ -3,26 +3,19 @@ module Main where
 import System.IO
 import Data.List
 
-countUnique :: [String] -> Int
-countUnique = length . foldl union "" . sort
-
-countShared :: [String] -> Int
-countShared = length . foldl intersect "abcdefghijklmnopqrstuvwxyz" . sort
-
-input :: IO [[String]]
-input = openFile "days/Inputs/Day6.txt" ReadMode >>= gather [] []
-    where gather acc1 acc2 handle = do
+apply :: (String -> String -> String) -> String -> IO ()
+apply f id = openFile "days/Inputs/Day6.txt" ReadMode >>= gather 0 id
+    where gather n s handle = do
             done <- hIsEOF handle
             if done
-                then return (acc2:acc1)
+                then putStrLn $ show $ n + length s
                 else do
                     line <- hGetLine handle
                     if line == ""
-                        then gather (acc2:acc1) [] handle
-                        else gather acc1 (line:acc2) handle
+                        then gather (n + length s) id handle
+                        else gather n (f line s) handle
 
 main :: IO ()
 main = do
-    groups <- input
-    putStrLn $ show $ sum $ map countUnique groups
-    putStrLn $ show $ sum $ map countShared groups
+    apply union ""
+    apply intersect "abcdefghijklmnopqrstuvwxyz"
